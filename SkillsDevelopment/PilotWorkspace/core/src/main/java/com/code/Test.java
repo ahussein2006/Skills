da4b44cc8d6f468f328983f0bb8bb33206784cbc
Ahmed Hassan <compeng.ahmedhassan@gmail.com>
2022-03-24 17:23:41 +0200

    Added core Project

---
+++ SkillsDevelopment/PilotWorkspace/core/src/main/java/com/code/Test.java
@@ -0,0 +1,11 @@
+package com.code;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Test {
+
+    public String getMessage() {
+	return "OK";
+    }
+}
--- SkillsDevelopment/PilotWorkspace/pilot/src/main/java/com/code/business/CustomersBusiness.java
+++ SkillsDevelopment/PilotWorkspace/pilot/src/main/java/com/code/business/CustomersBusiness.java
@@ -1,11 +1,17 @@
 package com.code.business;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.code.Test;
 
 @Service
 public class CustomersBusiness {
 
+    @Autowired
+    Test test;
+
     public String getCustomers() {
-	return "OK";
+	return test.getMessage();
     }
 }
